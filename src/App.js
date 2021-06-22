4e79250e79608eb7729e8cb29bf44e82eef858ee
slim8020 <slim8020@naver.com>
2021-06-22 15:59:09 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+
+const App = () => {
+  return (
+   <View style={styles.container}>
+     <Text style={styles.title}>My First React Native</Text>
+   </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  title:{
+    fontSize: 30,
+  },
+});
+
+export default App;
